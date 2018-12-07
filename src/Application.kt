bfa9bea5f9c4f3808b3408239c4cc7573abb0c66
James <JamesStumme@gmail.com>
2018-12-06 19:24:47 -0600

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package uk.stumme
+
+import io.ktor.application.*
+import io.ktor.routing.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+    }
+}
