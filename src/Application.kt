280bdc1e637e1d3b15f5d9c471d1f76e23f94200
James <JamesStumme@gmail.com>
2018-12-10 21:37:28 -0600

    Setup app mains thread.

--- src/Application.kt
+++ src/Application.kt
@@ -1,22 +1,24 @@
 package uk.stumme
 
-import io.ktor.application.*
-import io.ktor.routing.*
-import io.ktor.gson.*
-import io.ktor.features.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
+import io.ktor.routing.routing
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import uk.stumme.account.AccountController
 import uk.stumme.account.AccountRepo
-
 import uk.stumme.account.accounts
 import uk.stumme.models.database.Account
+import uk.stumme.models.database.Transfers
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.addFactory
-import uy.kohesive.injekt.api.addSingleton
 import uy.kohesive.injekt.api.get
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -24,8 +26,16 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     install(ContentNegotiation) {
         gson {
+            setPrettyPrinting()
         }
     }
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.respond(HttpStatusCode.InternalServerError, "Internal Server Error")
+            throw cause
+        }
+    }
+
     Injekt.addFactory { AccountRepo() }
     Injekt.addFactory { AccountController(Injekt.get()) }
 
@@ -40,6 +50,6 @@ private fun initDb() {
     Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "org.h2.Driver")
 
     transaction {
-        SchemaUtils.create(Account)
+        SchemaUtils.create(Account, Transfers)
     }
 }
